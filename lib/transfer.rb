09be32bc6803c5cce9818a0b24655f6fb9116fb6
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-11 15:29:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :status
   
-  def initialize(sender, receiver, status = "pending")
+  def initialize(sender, receiver, status)
    @sender = sender
    @receiver = receiver
-   @status = status
+   @status = "pending"
   end  
 end
